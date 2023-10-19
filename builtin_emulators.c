5682a88f1bd586e917125ce89fdd5dc1ed25dfca
ANAS AHMED <86914300+baghdadsniper@users.noreply.github.com>
2023-10-19 22:59:41 +0300

    zxczxs

--- builtin_emulators.c
+++ builtin_emulators.c
@@ -7,7 +7,7 @@
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
-int _myexit(info_t *info)
+int _myyexit(info_t *info)
 {
 	int exitcheck;
 
@@ -35,7 +35,7 @@ int _myexit(info_t *info)
  *          constant function prototype.
  *  Return: Always 0
  */
-int _mycd(info_t *info)
+int _myycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
 	int chdir_ret;
@@ -85,7 +85,7 @@ int _mycd(info_t *info)
  *          constant function prototype.
  *  Return: Always 0
  */
-int _myhelp(info_t *info)
+int _myshelp(info_t *info)
 {
 	char **arg_array;
 
